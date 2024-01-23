fa057da2d3ab2057a66e40885262aa1f4acc03ed
Anghofal <mfishy25@gmail.com>
2024-01-23 16:29:08 +0700

    stove counter continue

---
+++ Assets/Scripts/Counters/BaseCounter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent
+{
+    [SerializeField] private Transform pointTopOfClearCounter;
+    private KitchenObject kitchenObject;
+
+    public virtual void Interact(Pemain pemain)
+    {
+        Debug.LogError("The function must be overrided");
+    }
+
+    public virtual void InteractAlternate(Pemain pemain)
+    {
+    }
+
+    public Transform GetKitchenObjectLocation()
+    {
+        return pointTopOfClearCounter;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+
+}
--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,10 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class StoveCounter : BaseCounter
 {
+    private enum State
+    {
+        Idle,
+        Frying,
+        Fried,
+        Burned
+    }
+
     [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+    private FryingRecipeSO fryingRecipeSO;
+    private float fryingTimer;
+    private State state;
+
+    private void Start()
+    {
+        state = State.Idle;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Frying:
+                if (HasKitchenObject())
+                {
+                    fryingTimer += Time.deltaTime;
+                    if (fryingTimer > fryingRecipeSO.timeCook)
+                    {
+                        GetKitchenObject().DestroySelf();
+                        KitchenObject.SpawnKitchenObject(fryingRecipeSO.output, this);
+                        state = State.Fried;
+                    }
+                }
+                break;
+            case State.Fried:
+                break;
+            case State.Burned:
+                break;
+        }
+        Debug.Log(state);
+
+        
+    }
 
     public override void Interact(Pemain pemain)
     {
@@ -15,8 +60,12 @@ public class StoveCounter : BaseCounter
             {
                 if (HasRecipe(pemain.GetKitchenObject().GetKitchenObjectSO()))
                 {
-
+                    
                     pemain.GetKitchenObject().SetKitchenObjectParent(this);
+                    fryingRecipeSO = GetFryRecipeSO(GetKitchenObject().GetKitchenObjectSO());
+                    fryingTimer = 0;
+                    state = State.Frying;
+
 
                 }
             }
@@ -65,13 +114,16 @@ public class StoveCounter : BaseCounter
 
     private FryingRecipeSO GetFryRecipeSO(KitchenObjectSO kitchenObjectSO)
     {
+        FryingRecipeSO fryingRecipeAny = null;
         foreach (FryingRecipeSO fryRecipeSO in fryingRecipeSOArray)
         {
+            fryingRecipeAny = fryRecipeSO;
             if (fryRecipeSO.input == kitchenObjectSO)
             {
                 return fryRecipeSO;
             }
         }
-        return null;
+        return fryingRecipeAny;
+        
     }
 }
